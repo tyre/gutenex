971e73a152e1112721b4e2e2c28028f3e8c677f1
Chris Maddox <tyre77@gmail.com>
2014-09-28 17:47:46 -0700

    clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -3,27 +3,17 @@ defmodule Gutenex.Mixfile do
 
   def project do
     [app: :gutenex,
-     version: "0.0.1",
+     name: "Gutenex",
+     version: "0.0.1-dev",
+     source_url: "https://github.com/SenecaSystems/gutenex",
      elixir: "~> 1.0.0",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       { :erlguten, github: "ztmr/erlguten" }
