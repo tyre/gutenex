c9b151e85b48f779ec8942e6a42cfaea75832ddd
Chris Maddox <tyre77@gmail.com>
2014-11-16 10:21:30 -0800

    0.1.0 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gutenex.Mixfile do
   def project do
     [app: :gutenex,
      name: "Gutenex",
-     version: "0.0.1-dev",
+     version: "0.1.0",
      source_url: "https://github.com/SenecaSystems/gutenex",
      elixir: "~> 1.0",
      deps: deps]
